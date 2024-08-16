27010c956c6c758f16fa91bfb2f41bcecea8ea21
adamfilipczuk <adamflippa@gmail.com>
2024-08-16 20:27:06 +1000

    Splash Screen

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
+        splashScreen splash1 = new splashScreen(2500);
+        splash1.showSplash();
+
         new Main_Menu();
     }
 }
---
+++ src/splashScreen.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class splashScreen extends JWindow {
+    private int duration;
+    public splashScreen(int duration)    {
+        this.duration = duration;
+    }
+    public void showSplash(){
+        JPanel contentPane = (JPanel)getContentPane();
+        contentPane.setBackground(Color.WHITE);
+
+        ImageIcon splashImage = new ImageIcon("resources/splash.gif");
+        int width = splashImage.getIconWidth();
+        int height = splashImage.getIconHeight();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screenSize.width - width) / 2;
+        int y = (screenSize.height - height) / 2;
+        setBounds(x, y, width, height);
+
+        // Create a JLayeredPane to layer components
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(width, height));
+
+        // Create and add splashBackground
+        JLabel splashBackground = new JLabel(new ImageIcon("resources/splash.gif"));
+        splashBackground.setBounds(0, 0, width, height); // Make sure it covers the whole area
+        layeredPane.add(splashBackground, JLayeredPane.DEFAULT_LAYER);
+
+        // Create and add splashName
+        JLabel splashName = new JLabel(new ImageIcon("resources/logo.png"));
+        splashName.setBounds(0, 0, width, height); // Make sure it covers the whole area
+        layeredPane.add(splashName, JLayeredPane.PALETTE_LAYER);
+
+        //Create and add progressBar
+        JProgressBar progressBar = new JProgressBar();
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+        progressBar.setBounds(0, height-30, width, 20);
+        layeredPane.add(progressBar, JLayeredPane.PALETTE_LAYER);
+
+        // Set the layeredPane as the content pane
+        setContentPane(layeredPane);
+        pack(); // Resize the window to fit the content
+
+        // Display the splash screen
+        setVisible(true);
+
+        // Simulate progress
+        for (int i = 0; i <= 100; i++){
+            try {
+                Thread.sleep(duration / 100); // Simulate something being done
+                progressBar.setValue(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        setVisible(false);
+    }
+    public void displayAndClose(){
+        showSplash();
+        System.exit(0);
+    }
+    public static void main(String[] args){
+        splashScreen splashScreen1 = new splashScreen(2500);
+        splashScreen1.displayAndClose();
+    }
+}
